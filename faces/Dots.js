cb341c28af3638444ecb3297b66ddc23f643eb58
Benjamin Howe <bdhowe@gmail.com>
2018-09-17 20:47:12 -0600

    refactor for performance

--- faces/Dots.js
+++ faces/Dots.js
@@ -2,19 +2,24 @@ const DOTS_COUNT = 850;
 const MS_PER_DAY = 86400000;
 
 export default class Dots {
-  constructor(context) {
+  constructor(context, canvasFactory) {
     this.context = context;
+    this.dimensions = {width: 0, height: 0, centerX: 0, centerY: 0, longestDimension: 0, shortestDimension: 0};
+    this.canvasFactory = canvasFactory;
+    this.backgroundCanvas = null;
+    this.dotCanvas = null;
+    this.setSize({width: context.canvas.width, height: context.canvas.height});
+    this.generateAssets();
   }
 
-  getDimensions() {
-    const context = this.context;
-    const width = context.canvas.width;
-    const height = context.canvas.height;
+  setSize(newSize) {
+    const {width, height} = newSize;
     const centerX = width / 2;
     const centerY = height / 2;
-    const longestDimension = centerX > centerY ? centerX : centerY;
-    const shortestDimension = centerX < centerY ? centerX : centerY;
-    return {width, height, centerX, centerY, longestDimension, shortestDimension};
+    const longestDimension = width > height ? width : height;
+    const shortestDimension = width < height ? width : height;
+    this.dimensions = {width, height, centerX, centerY, longestDimension, shortestDimension};
+    this.generateBackground();
   }
 
   getTargetTimestamp(time) {
@@ -27,10 +32,15 @@ export default class Dots {
     return targetDate.getTime();
   }
 
-  drawBackground() {
-    const context = this.context;
-    const {centerX, centerY, longestDimension, width, height} = this.getDimensions();
-    const gradient = context.createRadialGradient(
+  generateBackground() {
+    const {width, height, centerX, centerY, longestDimension} = this.dimensions;
+    this.backgroundCanvas = this.backgroundCanvas || this.canvasFactory.getCanvas();
+    this.backgroundCanvas.width = width;
+    this.backgroundCanvas.height = height;
+
+    const bgContext = this.backgroundCanvas.getContext('2d');
+
+    const gradient = bgContext.createRadialGradient(
       centerX,
       centerY,
       0,
@@ -38,40 +48,61 @@ export default class Dots {
       centerY,
       longestDimension
     );
+
     gradient.addColorStop(0.2, "rgb(0,25,56)");
     gradient.addColorStop(1, "rgb(0,64,142)");
-    context.fillStyle = gradient;
-    context.rect(0, 0, width, height);
-    context.fill();
+    bgContext.fillStyle = gradient;
+    bgContext.rect(0, 0, width, height);
+    bgContext.fill();
+  }
+
+  generateDot() {
+    this.dotCanvas = this.dotCanvas || this.canvasFactory.getCanvas();
+    this.dotCanvas.width = 3;
+    this.dotCanvas.height = 3;
+
+    const dotContext = this.dotCanvas.getContext('2d');
+
+    const gradient = dotContext.createRadialGradient(1.5, 1.5, 0, 1.5, 1.5, 1.5);
+    gradient.addColorStop(0.4, `rgba(180,180,180,1.0)`);
+    gradient.addColorStop(1, `rgba(180,180,180,0.0)`);
+    dotContext.fillStyle = gradient;
+
+    dotContext.beginPath();
+    dotContext.arc(1.5, 1.5, 1.5, 0, 2 * Math.PI);
+    dotContext.fill();
+  }
+
+  generateAssets() {
+    this.generateBackground();
+    this.generateDot();
+  }
+
+  drawBackground() {
+    const context = this.context;
+    const backgroundCanvas = this.backgroundCanvas;
+    context.drawImage(backgroundCanvas, 0, 0);
   }
 
   drawDots(progress) {
-    const {longestDimension, width, height} = this.getDimensions();
+    const {longestDimension, width, height} = this.dimensions;
     const context = this.context;
     const arcMultiplier = progress * -1;
     const radiusMultiplier = longestDimension / DOTS_COUNT;
 
     for (var i = 0; i < DOTS_COUNT; i++) {
-      const blue = (255 - 180) * (i / DOTS_COUNT) + 180;
       const radius = i * radiusMultiplier;
       const arc = i * arcMultiplier * 2 * Math.PI;
       const x = Math.sin(arc) * radius + width / 2;
       const y = Math.cos(arc) * radius + height / 2;
 
-      const gradient = context.createRadialGradient(x, y, 0, x, y, 1.5);
-      gradient.addColorStop(0.4, `rgba(180,180,${blue},1.0)`);
-      gradient.addColorStop(1, `rgba(180,180,${blue},0.0)`);
-      context.fillStyle = gradient;
-
-      context.beginPath();
-      context.arc(x, y, 1.5, 0, 2 * Math.PI);
-      context.fill();
+      context.drawImage(this.dotCanvas, x - 1.5, y - 1.5);
     }
   }
 
   drawTime() {
     const context = this.context;
-    const { height } = this.getDimensions();
+    const { height } = this.dimensions;
     const timeString = new Date().toLocaleTimeString();
     context.font = "2em Raleway";
     context.fillStyle = "rgba(255,255,255,0.8)";
--- script.js
+++ script.js
@@ -4,12 +4,19 @@ import Clock from "./Clock.js";
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-const dots = new Dots(context);
+const canvasFactory = {
+  getCanvas: () => {
+    return document.createElement('canvas');
+  }
+};
+
+const dots = new Dots(context, canvasFactory);
 const clock = new Clock();
 
 const resizeCanvas = function() {
   canvas.width = window.document.body.clientWidth;
   canvas.height = window.document.body.clientHeight;
+  dots.setSize({width:canvas.width, height:canvas.height});
 };
 
 window.addEventListener("resize", resizeCanvas);
